f21fbbaa50a945d8a776de8e1e14567817cc8a13
bruleboy22 <steve_e_01@yahoo.com>
2023-11-11 19:17:57 -0600

    got users to create on my database

--- teledogter-app/src/SignUp.jsx
+++ teledogter-app/src/SignUp.jsx
@@ -1,107 +1,144 @@
 import React, { useState } from 'react';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
-import Button from '@mui/material/Button';
+import axios from 'axios';
+import {
+  Box,
+  TextField,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  Button
+} from '@mui/material';
 
 export default function SignUp() {
-    const [role, setRole] = useState('');
-    const [username, setUsername] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [phoneNumber, setPhoneNumber] = useState('');
-    const [address, setAddress] = useState('');
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [role, setRole] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [address, setAddress] = useState('');
+  const [name, setName] = useState('');
 
-    const handleChange = (event) => {
-        setRole(event.target.value);
-    };
+  const handleChange = (setter) => (event) => setter(event.target.value);
 
-    const handleChangeUsername = (event) => {
-      setUsername(event.target.value);
-    }
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        console.log("Form submitted", { role });
-    };
+    axios.post('http://localhost:8080/api/users/create', {
+      userName: username,
+      emailId: email,
+      password,
+      role,
+      profile: {
+        name,
+        phone: phoneNumber,
+        address,
+        profile_picture: '',
+      }
+    })
+    .then((response) => console.log(response))
+    .catch((error) => console.log(error));
+  };
 
-
-    console.log('username: ', username);
-
-    return (
-        <Box
-            component="form"
-            onSubmit={handleSubmit}
-            sx={{
-              '& .MuiTextField-root': { m: 1, width: '25ch', bgcolor: '#333333', color: 'orange', '& input': { color: 'orange' }, '& label': { color: 'orange' }, '& .MuiOutlinedInput-root': { '& fieldset': { borderColor: 'orange' }, '&:hover fieldset': { borderColor: 'orange' }, '&.Mui-focused fieldset': { borderColor: 'orange' } } },
-              paddingTop: '64px',
+  return (
+    <Box
+      component="form"
+      onSubmit={handleSubmit}
+      sx={{
+        '& .MuiTextField-root': { 
+          m: 1, 
+          width: '25ch', 
+          bgcolor: '#333333', 
+          color: 'orange', 
+          '& input': { color: 'orange' }, 
+          '& label': { color: 'orange' }, 
+          '& .MuiOutlinedInput-root': { 
+            '& fieldset': { borderColor: 'orange' }, 
+            '&:hover fieldset': { borderColor: 'orange' }, 
+            '&.Mui-focused fieldset': { borderColor: 'orange' } 
+          } 
+        },
+        paddingTop: '64px',
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <div>
+        <TextField
+          required
+          id="user-name"
+          label="User Name Required"
+          variant="filled"
+          value={username}
+          onChange={handleChange(setUsername)}
+        />
+        <TextField
+          required
+          id="email-required"
+          label="Email Required"
+          variant="filled"
+          value={email}
+          onChange={handleChange(setEmail)}
+        />
+        <TextField
+          id="password-required"
+          label="Password Required"
+          type="password"
+          autoComplete="current-password"
+          variant="filled"
+          value={password}
+          onChange={handleChange(setPassword)}
+        />
+        <FormControl variant="filled" sx={{ m: 1, minWidth: 120 }}>
+          <InputLabel id="role-label">Role</InputLabel>
+          <Select
+            labelId="role-label"
+            id="role-select"
+            value={role}
+            onChange={handleChange(setRole)}
+            label="Role"
+          >
+            <MenuItem value={'Owner'}>Owner</MenuItem>
+            <MenuItem value={'Veterinarian'}>Veterinarian</MenuItem>
+          </Select>
+        </FormControl>
+        <TextField
+          id="phone-number"
+          label="Phone Number"
+          type="tel"
+          variant="filled"
+          helperText="Please enter your phone number"
+          value={phoneNumber}
+          onChange={handleChange(setPhoneNumber)}
+        />
+        <TextField
+          id="address"
+          label="Address"
+          variant="filled"
+          helperText="Please enter your address"
+          value={address}
+          onChange={handleChange(setAddress)}
+        />
+        <TextField
+          id="Name"
+          label="Name"
+          variant="filled"
+          value={name}
+          onChange={handleChange(setName)}
+        />
+        <Button 
+          type="submit" 
+          variant="contained" 
+          sx={{ 
+            bgcolor: '#333333', 
+            color: 'orange', 
+            '&:hover': { bgcolor: 'darkgray' } 
           }}
-            noValidate
-            autoComplete="off"
         >
-            <div>
-                <TextField
-                    required
-                    id="user-name"
-                    label="User Name Required"
-                    defaultValue="User Name"
-                    variant="filled"
-                    value={username}
-                    onChange={handleChangeUsername}
-                />
-                <TextField
-                    required
-                    id="email-required"
-                    label="Email Required"
-                    defaultValue="Email"
-                    variant="filled"
-                />
-                <TextField
-                    id="password-required"
-                    label="Password Required"
-                    type="password"
-                    autoComplete="current-password"
-                    variant="filled"
-                />
-                <FormControl variant="filled" sx={{ m: 1, minWidth: 120 }}>
-                    <InputLabel id="role-label">Role</InputLabel>
-                    <Select
-                        labelId="role-label"
-                        id="role-select"
-                        value={role}
-                        onChange={handleChange}
-                        label="Role"
-                    >
-                        <MenuItem value={'Owner'}>Owner</MenuItem>
-                        <MenuItem value={'Veterinarian'}>Veterinarian</MenuItem>
-                    </Select>
-                </FormControl>
-                <TextField
-                    id="Name"
-                    label="Name"
-                    type="search"
-                    variant="filled"
-                />
-                <TextField
-                    id="phone-number"
-                    label="Phone Number"
-                    type="tel"
-                    variant="filled"
-                    helperText="Please enter your phone number"
-                />
-                <TextField
-                    id="address"
-                    label="Address"
-                    variant="filled"
-                    helperText="Please enter your address"
-                />
-               <Button type="submit" variant="contained" sx={{ bgcolor: '#333333', color: 'orange', '&:hover': { bgcolor: 'darkgray' } }}>
-                    Submit
-                </Button>
-            </div>
-        </Box>
-    );
+          Submit
+        </Button>
+      </div>
+    </Box>
+  );
 }
+
